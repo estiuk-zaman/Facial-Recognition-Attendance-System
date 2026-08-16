12b72868783a253e7a4ab0dc8441713bd0f96995
estiuk <mdestiukuzzaman@gmail.com>
2026-08-16 09:29:13 +0600

    download DNN model

---
+++ download_dnn_model.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+print("Downloading prototxt...")
+urllib.request.urlretrieve("https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt", "deploy.prototxt")
+
+print("Downloading caffemodel...")
+urllib.request.urlretrieve("https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel", "res10_300x300_ssd_iter_140000.caffemodel")
+
+print("Download Complete!")

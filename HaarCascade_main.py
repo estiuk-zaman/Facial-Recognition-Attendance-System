975b18c21a33c95f765b4e669463d0985dccafb5
estiuk <mdestiukuzzaman@gmail.com>
2026-08-16 09:27:06 +0600

    Rejecting HaarCascade model for low performance and inaccuracy

--- HaarCascade_main.py
+++ HaarCascade_main.py
@@ -4,7 +4,8 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces= face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6, minSize=(50, 50))
+    gray = cv2.equalizeHist(gray)
+    faces= face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5, minSize=(90, 90))
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.imshow('Face Detection', frame)
